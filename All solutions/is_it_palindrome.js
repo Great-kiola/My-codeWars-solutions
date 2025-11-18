c078e241356527365108e1891d36a2f522512622
Ola <hollabodey.kilaso@gmail.com>
2025-11-18 14:26:19 +0000

    Added best method to the palindrome file

--- All solutions/is_it_palindrome.js
+++ All solutions/is_it_palindrome.js
@@ -1,12 +1,20 @@
 function isitpalindrome(x){
-    let str = "";
-    for(let i = x.length - 1; i >= 0; i--){
-        str += x[i]
-    }
+    // let str = "";
 
-    return str.toLowerCase() === x.toLowerCase() ? true : false
-    // console.log(str)
+    /* 
+        First we loop the string backwards
+        then add the looped to a new string variable
+    */
+    // for(let i = x.length - 1; i >= 0; i--){
+    //     str += x[i]
+    // }
+
+    // then compare the lowercase version of the new string with the previous string
+    // return str.toLowerCase() === x.toLowerCase() ? true : false
     
+    
+    //* Best Method
+    return x.split(" ").reverse().join().toLowerCase() === x.toLowerCase() ? true : false
 }
 
 console.log(isitpalindrome("Mom"))
