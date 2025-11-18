a85759212f45ec6bc6cca7821e430c409d812d2f
ola <hollabodey.kilaso@gmail.com>
2025-11-18 22:33:17 +0000

    Add stringToNumber function to convert string to number

---
+++ All solutions/8kyu/str2num.js
@@ -0,0 +1,14 @@
+/* 
+    This kata wants us to convert a string to a number
+*/
+
+const stringToNumber = function (str) {
+    // return Number(str);
+
+
+    // Other methods
+    // return +str
+
+    // b
+    return parseInt(str)
+}
