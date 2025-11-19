bf2d9e2a2eb37cacfe6a0dc67d622821564b2549
ola <hollabodey.kilaso@gmail.com>
2025-11-19 08:26:01 +0000

    Refactor countPositivesSumNegatives function for improved readability

--- All solutions/8kyu/countPositiveSumNegative.js
+++ All solutions/8kyu/countPositiveSumNegative.js
@@ -1,11 +1,23 @@
+/*
+  In this kata you are asked to count the number of positive value in an array
+  and sum the negatives, then add both the positive and negative to an array
+
+  condition
+  If the array is null or empty it should return and empty array
+*/
+
 function countPositivesSumNegatives(input) {
 
-    if (input == null || input.length === 0){
-      return []
-    }
-  
-    const positives = input.filter((e) => e > 0).length
-    const negatives = input.filter((f) => f < 0).reduce((a, b) => a + b, 0)
-    
-    return [positives, negatives]
+  // Check if the array is null or empty first
+  if (input == null || input.length === 0) {
+    return []
+  }
+
+  // If not filter out the positive value
+  const positives = input.filter((e) => e > 0).length
+  // filter out the negative value and perform the sum of the negative
+  const negatives = input.filter((f) => f < 0).reduce((a, b) => a + b, 0)
+
+  // return an array of both the positive and negative
+  return [positives, negatives]
 }
