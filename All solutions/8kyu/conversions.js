7ce8a667fec836b91a518429d25fd13ff66c1254
Ola <hollabodey.kilaso@gmail.com>
2025-11-20 02:23:36 +0000

    Add conversions function to handle type conversions between boolean and string, and string to number

---
+++ All solutions/8kyu/conversions.js
@@ -0,0 +1,14 @@
+// This kata would contain all conversions
+
+
+function conversions (x){
+
+    // Boolean to string
+    // return x.toString()
+    // return String(x)
+
+    // String to number
+    return Number(x)
+
+
+}
