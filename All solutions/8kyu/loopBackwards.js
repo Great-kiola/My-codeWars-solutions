56b265587b271459d6b490c26bafb2fee3cfb00a
ola <hollabodey.kilaso@gmail.com>
2025-11-21 18:43:43 +0000

    Add lovefunc to determine if two flowers are in love based on petal count

--- All solutions/8kyu/loopBackwards.js
+++ All solutions/8kyu/loopBackwards.js
@@ -1,10 +1,16 @@
 function backwardsLoop(e){
 
-    for (let i = e.length - 1; i >= 0 ; i--){
-        console.log(e[i])
+    // for (let i = e.length - 1; i >= 0 ; i--){
+    //     console.log(e[i])
+    // }
+
+    // Print Numbers    
+    for(let i = e; i >= 0; i--){
+        console.log(i)
     }
 
 }
 
 
-backwardsLoop([1, 2, 3, 4, 5])
+// backwardsLoop([1, 2, 3, 4, 5])
+backwardsLoop(3)
---
+++ All solutions/8kyu/loveFunc.js
@@ -0,0 +1,21 @@
+/*
+    Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't
+*/
+
+function lovefunc(flower1, flower2){
+  // moment of truth
+  
+//   if (flower1 % 2 == 0 && flower2 % 2 !== 0){
+//     return true
+//   } else if (flower1 % 2 == 0 && flower2 % 2 == 0) {
+//     return false
+//   } else if (flower1 % 2 !== 0 && flower2 % 2 == 0){
+//     return true
+//   } else {
+//     return false
+//   }
+
+
+  // Best methods
+    return flower1 % 2 !== flower2 % 2;
+}
