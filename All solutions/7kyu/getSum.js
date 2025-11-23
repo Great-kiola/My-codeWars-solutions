d94881812ae6179ccba5a20ce8760c87bb6fb0a3
ola <hollabodey.kilaso@gmail.com>
2025-11-23 18:38:08 +0000

    Add getSum function to calculate the sum of all integers between two numbers

---
+++ All solutions/7kyu/getSum.js
@@ -0,0 +1,28 @@
+function getSum(a, b) {
+    let minimum
+    let max
+
+    let sum = 0
+
+    if (a < b) {
+        minimum = a
+        max = b
+    } else {
+        max = a
+        minimum = b
+    }
+
+    for (let i = minimum; i <= max; i++) {
+        console.log(i)
+        sum += i
+    }
+
+    return sum
+
+    // Best Method
+    // let min = Math.min(a, b),
+    //     max = Math.max(a, b);
+    // return (max - min + 1) * (min + max) / 2;
+}
+
+console.log(getSum(2, 3)) 
--- All solutions/8kyu/playsBanjo.js
+++ All solutions/8kyu/playsBanjo.js
@@ -12,3 +12,4 @@ function areYouPlayingBanjo(name) {
     return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
 
 }
+
