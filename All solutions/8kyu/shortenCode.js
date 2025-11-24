74a4ea2ee8ef60c00e020ca0a4fba08775cfe2fc
ola <hollabodey.kilaso@gmail.com>
2025-11-24 03:02:25 +0000

    Refactor describeAge function for improved readability

--- All solutions/8kyu/shortenCode.js
+++ All solutions/8kyu/shortenCode.js
@@ -3,7 +3,11 @@
     to 137 characters max
 */
 
-function describeAge(age) {  
-  let y="You're a(n) "
-  return y+(age<13?"kid":age<18?"teenager":age<65?"adult":"elderly");
+function describeAge(age) {
+    let y = "You're a(n) "
+    return y + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly");
+
+    // can also be reduced further
+    // return "You're a(n) "  + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly");
+
 }
