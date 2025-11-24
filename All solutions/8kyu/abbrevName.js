a5ac1d0d3758412af0eff068209554281969854a
ola <hollabodey.kilaso@gmail.com>
2025-11-24 01:40:25 +0000

    Add function to convert a name into initials

---
+++ All solutions/8kyu/abbrevName.js
@@ -0,0 +1,19 @@
+/**
+ * in this kata we are asked to
+ * Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+ * The output should be two capital letters with a dot separating them.
+ * It should look like this: Sam Harris => S.H
+ * 
+ */
+
+
+function abbrevName(name) {
+
+    let nameSplit = name.split(" ")
+    let firstinitial = nameSplit[0][0], secondinitial = nameSplit[1][0]
+    console.log((firstinitial + "." + secondinitial).toUpperCase())
+
+}
+
+abbrevName("Sam Harris")
+abbrevName("Jake paul")
