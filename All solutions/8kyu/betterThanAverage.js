0854dc671c99f8abd825d8235e7b9e6cdbca6a7f
ola <hollabodey.kilaso@gmail.com>
2025-11-25 14:34:31 +0000

    Add betterThanAverage function to determine if a score is above the class average

---
+++ All solutions/8kyu/betterThanAverage.js
@@ -0,0 +1,18 @@
+/*
+    In this kata, we need to
+    check if your point is better than the class avarage
+*/
+
+function betterThanAverage(classPoints, yourPoints) {
+    // Your code here
+
+    let classSum = classPoints.reduce((a, b) => a + b, 0) + yourPoints
+    let classTotal = classPoints.length + 1
+    let classAverage = classSum / classTotal
+
+    return yourPoints > classAverage ? true : false
+
+    // Better Method
+    return yourPoints > classPoints.reduce((a, b) => a + b, 0) / classPoints.length;
+
+}
