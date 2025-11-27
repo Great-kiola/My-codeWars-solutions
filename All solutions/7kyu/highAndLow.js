a6dd4c474da207435581dc18d76adc4dabd41797
ola <hollabodey.kilaso@gmail.com>
2025-11-27 11:31:51 +0000

    Refactor highAndLow function for improved readability and maintainability

--- All solutions/7kyu/highAndLow.js
+++ All solutions/7kyu/highAndLow.js
@@ -4,11 +4,20 @@
  Examples Input: "1 2 3 4 5"   =>  Output: "5 1"
 */
 
-function highAndLow(numbers){
-  let removeSpaces =  numbers.split(" ")
-  let conv = removeSpaces.map((e) => Number(e))
+function highAndLow(numbers) {
+    let removeSpaces = numbers.split(" ")
+    let conv = removeSpaces.map((e) => Number(e))
+
+    const sort = conv.sort((a, b) => a - b)
     
-  const sort = conv.sort((a, b) => a -b)  
-  let highest = sort[sort.length - 1].toString() , lowest = sort[0].toString()
-  return `${highest} ${lowest}`
+    let highest = sort[sort.length - 1].toString(),
+    lowest = sort[0].toString()
+    
+    return `${highest} ${lowest}`
+
+    // Best Method
+    // numbers = numbers.split(' ');
+    // return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
 }
+
+console.log(highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"))
