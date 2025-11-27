33de346cefaa087dbd28155326b6a3bf3d7fff8f
ola <hollabodey.kilaso@gmail.com>
2025-11-27 11:29:45 +0000

    Add highAndLow function to return highest and lowest numbers from a space-separated string

---
+++ All solutions/7kyu/highAndLow.js
@@ -0,0 +1,14 @@
+/*
+ In this kata, we are 
+ given a string of space separated numbers, and have to return the highest and lowest number.
+ Examples Input: "1 2 3 4 5"   =>  Output: "5 1"
+*/
+
+function highAndLow(numbers){
+  let removeSpaces =  numbers.split(" ")
+  let conv = removeSpaces.map((e) => Number(e))
+    
+  const sort = conv.sort((a, b) => a -b)  
+  let highest = sort[sort.length - 1].toString() , lowest = sort[0].toString()
+  return `${highest} ${lowest}`
+}
