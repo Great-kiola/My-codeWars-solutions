b98942514b5ff396b179e50945f3ce3d3daae664
ola <hollabodey.kilaso@gmail.com>
2025-11-28 23:26:00 +0000

    Fix formatting issues in getMiddle function and update test case

--- All solutions/7kyu/getMiddle.js
+++ All solutions/7kyu/getMiddle.js
@@ -9,7 +9,7 @@
 function getMiddle(s) {
 
     let n = s.length
-    return n%2 === 0 ? s[n/2-1]+s[n/2]:s[Math.floor[n/2]]
+    return n % 2 === 0 ? s[n/2 - 1] + s[n/2] : s[Math.floor(n/2)]
 }
 
-console.log(getMiddle("Test"))
+console.log(getMiddle("Testy"))
