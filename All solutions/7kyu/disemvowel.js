c1f7dbd2cd60f0184555aaa12323d9ef94b24d22
ola <hollabodey.kilaso@gmail.com>
2025-11-28 17:28:49 +0000

    Add disemvowel function to remove vowels from a string

---
+++ All solutions/7kyu/disemvowel.js
@@ -0,0 +1,29 @@
+/*
+    In this kata, we are asked to
+    write a function that takes a string and return a new string with all vowels removed.
+    For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+*/
+
+
+function disemvowel(str) {
+
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    let wordsplit = str.split("")
+
+    let wordString = "";
+
+
+    for (let i = 0; i < wordsplit.length; i++) {
+        if (!vowels.includes(wordsplit[i])) {
+            wordString += wordsplit[i]
+        }
+    }
+
+    return wordString
+
+
+    // Best Method
+    // return str.replace(/[aeiou]/gi, '');
+
+
+}
