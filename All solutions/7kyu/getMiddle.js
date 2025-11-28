d1b0e77dc3ef410627c7cb21ab73070079b8d8ad
ola <hollabodey.kilaso@gmail.com>
2025-11-28 23:23:01 +0000

    Update getMiddle function documentation for clarity and usage examples

--- All solutions/7kyu/getMiddle.js
+++ All solutions/7kyu/getMiddle.js
@@ -1,7 +1,10 @@
-// function getMiddle(s){
-//   const n = s.length;
-//   return n % 2 === 0 ? s[n/2 - 1] + s[n/2] : s[Math.floor(n/2)];
-// }
+/*
+    You are going to be given a non-empty string. Your job is to return the middle character(s) of the string.
+    If the string's length is odd, return the middle character.
+    If the string's length is even, return the middle 2 characters.
+
+    i.e: "test" --> "es"
+*/
 
 function getMiddle(s) {
 
