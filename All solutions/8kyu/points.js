65a20dacb056bd95a0235631fefa2d035b7c0dc6
ola <hollabodey.kilaso@gmail.com>
2025-11-29 01:24:01 +0000

    Refactor points function by commenting out old implementation of total points calculation

--- All solutions/8kyu/points.js
+++ All solutions/8kyu/points.js
@@ -22,13 +22,15 @@ function points(games) {
     }, 0);
 
 
-//     let total = 0;
-//     games.map(game => {
-//         if (game[0] === game[2]) {
-//             total += 1;
-//         } else if (game[0] > game[2]) {
-//             total += 3;
-//         }
-//     });
-//     return total;
+
+    // Best Method
+    //     let total = 0;
+    //     games.map(game => {
+    //         if (game[0] === game[2]) {
+    //             total += 1;
+    //         } else if (game[0] > game[2]) {
+    //             total += 3;
+    //         }
+    //     });
+    //     return total;
 }
