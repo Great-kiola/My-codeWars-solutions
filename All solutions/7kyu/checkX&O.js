8a134be4932f59b3e8d7fb73be9f065809f2f0e6
ola <hollabodey.kilaso@gmail.com>
2025-12-02 10:15:29 +0000

    Fix formatting in XO function documentation for clarity

--- All solutions/7kyu/checkX&O.js
+++ All solutions/7kyu/checkX&O.js
@@ -1,6 +1,7 @@
 /*
     In this kata we are asked to
-    Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+    Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive.
+    The string can contain any char.
 
     Examples input/output: XO("ooxx") => true
 */
