24a8ae987be271e58651f96db9cc4c2b512a4602
ola <hollabodey.kilaso@gmail.com>
2025-12-02 10:16:12 +0000

    Refactor XO function for improved readability and consistency

--- All solutions/7kyu/checkX&O.js
+++ All solutions/7kyu/checkX&O.js
@@ -7,16 +7,20 @@
 */
 
 function XO(str) {
-  let countO = 0
-  let countX = 0
-  
-  let lower = str.toLowerCase()  
-  for(let i=0; i< lower.length;i++){
-    if(lower[i] == "x"){
-      countX++
-    } else if(lower[i] == "o"){
-      countO++
+    let countO = 0
+    let countX = 0
+
+    let lower = str.toLowerCase()
+    for (let i = 0; i < lower.length; i++) {
+        if (lower[i] == "x") {
+            countX++
+        } else if (lower[i] == "o") {
+            countO++
+        }
     }
-  }
-  return countO == countX
+    return countO == countX
+
+    //Best Method
+    str = str.toLowerCase().split('');
+    return str.filter(x => x === 'x').length === str.filter(x => x === 'o').length;
 }
