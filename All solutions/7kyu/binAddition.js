51aca720ff70daf62d3248915766d3f695792621
ola <hollabodey.kilaso@gmail.com>
2025-12-06 19:58:37 +0000

    Add addBinary function to return the sum of two numbers in binary format

---
+++ All solutions/7kyu/binAddition.js
@@ -0,0 +1,30 @@
+/*
+    In this kata, we are asked to 
+    Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+    The binary number returned should be a string.
+
+    i.e 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+*/
+
+function addBinary(a, b) {
+    // return (a + b).toString(2)
+
+    // Solution that doesnt use toString()
+    var sum = a + b;
+    var binary = [];
+
+    var x = 0, y = 0;
+
+    y = sum;
+    while (y > 1) {
+        x = y % 2;
+        y = Math.floor(y / 2);
+        binary.push(x);
+    };
+
+    binary.push(y);
+    return binary.reverse().join("");
+}
+
+
+console.log(addBinary(1,1))
