d449b70f9dfb1a43fd4af6ce6ae5f4e4d630df59
ola <hollabodey.kilaso@gmail.com>
2025-12-19 18:26:11 +0000

    Add forEach, for of, and for in examples to demonstrate array iteration

---
+++ All solutions/forss.js
@@ -0,0 +1,38 @@
+// difference between foreach, for of, and for in
+
+
+let arr = [2,3,7,8,6]
+let obj  = {
+    fname: "ola",
+    lname: "Kilaso"
+}
+
+/*
+    in for loop and for in you get access to the index in the array or object,
+    not the actual element
+*/
+
+// for loop:
+// for (let i = arr.length -1 ; i >= 0; i--){
+//     console.log(arr[i])
+// }
+
+// for in
+for (let q in arr){
+    console.log(q)
+
+}
+
+/*
+    In for each and for of 
+*/
+
+// for Each
+// arr.forEach((num) => {
+//     console.log(num)
+// })
+
+// for of
+for(let key of arr){
+    console.log(key)
+}
