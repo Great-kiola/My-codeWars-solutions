968c3e865165acce53d6f38026fd84d5150967e5
ola <hollabodey.kilaso@gmail.com>
2025-12-25 02:06:58 +0000

    Add altSquareSum function to calculate the sum of squares at even positions and sum of odd positioned elements

---
+++ All solutions/7kyu/altSquareSum.js
@@ -0,0 +1,10 @@
+/*
+    In this kata we are asked to Complete the function that takes an array of integers as input
+    and finds the sum of squares of the elements at even positions (i.e. 2nd, 4th, etc.) plus the
+    sum of the rest of the elements at odd position.
+
+    [11, 12, 13, 14, 15]  -->  379
+    # 1.  2.  3.  4.  5.  position
+
+    11 + 12^2 + 13 + 14^2 + 15 = 379
+*/
