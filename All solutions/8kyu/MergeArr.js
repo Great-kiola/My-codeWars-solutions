3efb148dd7ba18487317c90917b594563a9c8586
ola <hollabodey.kilaso@gmail.com>
2025-12-28 00:58:39 +0000

    Update documentation and improve mergeArrays function implementation

--- All solutions/8kyu/MergeArr.js
+++ All solutions/8kyu/MergeArr.js
@@ -5,5 +5,16 @@
     - The resulting array is sorted in ascending order.
     - Any duplicate values are removed, so each integer appears only once.
     - If both input arrays are empty, return an empty array.
+
+    i.e :  [1, 3, 5, 7, 9], [10, 8, 6, 4, 2] -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 */
 
+function mergeArrays(arr1, arr2) {
+    let newArr = [...arr1, ...arr2]
+    let dupArr = new Set(newArr)
+    return [...dupArr].sort((a, b) => a - b)
+
+    //Best Method
+    // return Array.from(new Set(arr1.concat(arr2).sort((a,b) => (a-b))));
+
+}
