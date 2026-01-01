5fb6d02142c7bcf880bfb9410d97266cb4a1571f
ola <hollabodey.kilaso@gmail.com>
2026-01-01 07:03:24 +0000

    Comment out howMuchILoveYou function and add findMissing function to calculate missing number in an array

--- All solutions/8kyu/howMuchILoveYou.js
+++ All solutions/8kyu/howMuchILoveYou.js
@@ -3,14 +3,35 @@
     you start over with "I love you" for 7 petals, "a little" for 8 petals and so on.
 */
 
-function howMuchILoveYou(nbPetals) {
-    const phrases = [
-        "I love you",
-        "a little",
-        "a lot",
-        "passionately",
-        "madly",
-        "not at all",
-    ];
-    return phrases[(nbPetals - 1) % phrases.length]
+// function howMuchILoveYou(nbPetals) {
+//     const phrases = [
+//         "I love you",
+//         "a little",
+//         "a lot",
+//         "passionately",
+//         "madly",
+//         "not at all",
+//     ];
+//     return phrases[(nbPetals - 1) % phrases.length]
+// }
+
+
+function findMissing(numbers){
+
+    let actualSum = 0
+    let expctedSum = 0
+
+    for(nums of numbers){
+        actualSum += nums
+    }
+
+    for(let i = 0; i <= numbers.length; i++){
+        expctedSum += i
+    }
+
+    return expctedSum - actualSum
 }
+
+console.log(findMissing([1,3,0]))
+// expectedSUM = 0 + 1 + 2 + 3 = 6 
+// ActualSum = 0 + 1 + 3 = 4
