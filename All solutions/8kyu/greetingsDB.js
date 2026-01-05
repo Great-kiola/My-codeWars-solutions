d4a35d629ec7dbe7253cb90eac013ad1a8df81f5
ola <hollabodey.kilaso@gmail.com>
2026-01-05 16:18:52 +0000

    Add greetingsDB.js with greeting messages and greet function

---
+++ All solutions/8kyu/greetingsDB.js
@@ -0,0 +1,29 @@
+/*
+    In this kata we need to return a messgae based on the string passed.
+*/
+
+var database = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+}
+
+function greet(language) {
+    return database[language] || "Welcome";
+}
+
+console.log(greet('welsh'))
