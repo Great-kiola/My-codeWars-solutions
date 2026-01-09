495232d2588e9e234717f37966d2f79680ff1064
ola <hollabodey.kilaso@gmail.com>
2026-01-09 15:41:27 +0000

    Add validParentheses function to check the validity of parentheses in a string

---
+++ All solutions/7kyu/validParenthesis.js
@@ -0,0 +1,25 @@
+/*
+    In this kata we are asked to Write a function that takes a
+    string of parentheses, and determines if the order of the parentheses is
+    valid. The function should return true if the string is valid, and false if it's invalid.
+
+    i.e: "()" => true , "(())((()())())"  =>  true
+*/
+
+function validParentheses(parenStr) {
+    let newArr = []
+
+    for (let i = 0; i < parenStr.length; i++) {
+        if (parenStr[i] == "(") {
+            newArr.push(")")
+        } else if (parenStr[i] == "{") {
+            newArr.push("}")
+        } else if (parenStr[i] == "[") {
+            newArr.push("]")
+        } else if (parenStr[i] !== newArr.pop()) {
+            return false
+        }
+    }
+
+    return newArr.length == 0
+}
