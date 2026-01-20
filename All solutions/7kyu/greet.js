90f036f333d5a73f325bd7f24d914b8b16075639
ola <hollabodey.kilaso@gmail.com>
2026-01-20 23:02:57 +0000

    Add greet function to return a greeting message or null for empty input

---
+++ All solutions/7kyu/greet.js
@@ -0,0 +1,13 @@
+/*
+    In this kata we are asked to write a function to greet a person.
+    Function will take name as input and greet the person by saying hello.
+    Return null/nil/None if input is empty string or null/nil/None.
+*/
+
+function greet(name) {
+  if(name === null || name.length == 0){
+    return null
+  } else {
+    return `hello ${name}!`
+  }
+}
