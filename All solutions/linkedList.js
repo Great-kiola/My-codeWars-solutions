fdaa5a70f42266eb0be6bef23d7f8b9a6f80482a
ola <hollabodey.kilaso@gmail.com>
2026-01-24 11:02:23 +0000

    Fix Node class initialization and enhance LinkedList print method

--- All solutions/linkedList.js
+++ All solutions/linkedList.js
@@ -1,7 +1,7 @@
 class Node{
     constructor(data){
         this.data = data
-        this.head = null
+        this.next = null
     }
 }
 
@@ -13,8 +13,9 @@ class LinkedList {
 
     append(data){
         const node = new Node(data)
-        if (this.head == null) this.head = node
-        else{
+        if (!this.head){
+            this.head = node
+        }else{
             let current = this.head
             while(current.next != null){
                 current = current.next
@@ -27,9 +28,28 @@ class LinkedList {
     isEmpty(){
         return this.length === 0
     }
+
+    print(){
+        let current = this.head;
+        let output = "";
+
+        while(current){
+            output += current.data + " -> ";
+            current = current.next
+        }
+
+        output += "null";
+        console.log(output)
+    }
+
 }
 
 
 const list = new LinkedList()
-console.log(list)
+list.append(20)
+list.append(25)
+list.append(50)
+list.print()
+
+
 console.log(list.isEmpty())
