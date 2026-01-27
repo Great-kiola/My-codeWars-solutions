045f66d8fdf79bdc7e25e8a103d839a0eabc9fbf
ola <hollabodey.kilaso@gmail.com>
2026-01-27 17:51:46 +0000

    Refactor arr2bin function to improve readability by uncommenting main logic and commenting out alternative implementation

--- All solutions/8kyu/arr2Bin.js
+++ All solutions/8kyu/arr2Bin.js
@@ -5,25 +5,25 @@
 
 function arr2bin(arr) {
 
-    // let sum = 0
-    // let validArr = []
+    let sum = 0
+    let validArr = []
 
     // Looped through the array, and found the value that is a number in the array and pushed it to
     // valid arr
-    // for (let i = 0; i < arr.length; i++) {
-    //     if (typeof arr[i] == 'number') {
-    //         validArr.push(arr[i])
-    //     }
-    // }
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] == 'number') {
+            validArr.push(arr[i])
+        }
+    }
 
-    // validArr.forEach((x) => {
-    //     sum += x
-    // })
+    validArr.forEach((x) => {
+        sum += x
+    })
 
-    // return sum.toString(2)
+    return sum.toString(2)
 
     // Best practice
-    return arr.reduce((x, y) => x + (typeof y == "number" ? y : 0), 0).toString(2);
+    // return arr.reduce((x, y) => x + (typeof y == "number" ? y : 0), 0).toString(2);
 
 
 }
