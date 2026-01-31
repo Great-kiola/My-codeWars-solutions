a88acd2a91a629fa7be53ba1a55024a35e614aef
ola <hollabodey.kilaso@gmail.com>
2026-01-31 15:44:49 +0000

    Refactor bandNameGenerator function to use the at() method for character access

--- All solutions/7kyu/bandNameGen.js
+++ All solutions/7kyu/bandNameGen.js
@@ -4,7 +4,7 @@
 
 // My method
 function bandNameGenerator(str) {
-    if (str[0] === str[str.length - 1]) {
+    if (str.at(0) === str.at(-1)) {
         return `${str.charAt(0).toUpperCase() + str.slice(1)}${str.slice(1)}`
     } else {
         return `The ${str.charAt(0).toUpperCase() + str.slice(1)}`
