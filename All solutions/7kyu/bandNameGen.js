912f7a301145e7996dca2620d04c2fe7915c70c2
ola <hollabodey.kilaso@gmail.com>
2026-01-31 15:43:41 +0000

    Uncomment bandNameGenerator function to restore its functionality

--- All solutions/7kyu/bandNameGen.js
+++ All solutions/7kyu/bandNameGen.js
@@ -3,10 +3,10 @@
 */
 
 // My method
-// function bandNameGenerator(str) {
-//     if (str[0] == str[str.length - 1]) {
-//         return `${str.charAt(0).toUpperCase() + str.slice(1)}${str.slice(1)}`
-//     } else {
-//         return `The ${str.charAt(0).toUpperCase() + str.slice(1)}`
-//     }
-// }
+function bandNameGenerator(str) {
+    if (str[0] == str[str.length - 1]) {
+        return `${str.charAt(0).toUpperCase() + str.slice(1)}${str.slice(1)}`
+    } else {
+        return `The ${str.charAt(0).toUpperCase() + str.slice(1)}`
+    }
+}
