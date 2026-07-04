d6432a647b025eff3b713835f46bfb10fffb2923
ola <hollabodey.kilaso@gmail.com>
2026-07-04 11:13:05 +0100

    Comment out date-related code and add console logs for current time

--- All solutions/dates.js
+++ All solutions/dates.js
@@ -1,6 +1,13 @@
-const today = new Date().getDay()
-const getDay = (dayIndex) => {
-  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-  return days[dayIndex]
-}
-console.log('today is: ' + getDay(today))
+// const today = new Date().getDay()
+// const getDay = (dayIndex) => {
+//   const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+//   return days[dayIndex]
+// }
+// console.log('today is: ' + getDay(today)) 
+
+
+
+console.log("Hello")
+
+const now = new Date()
+console.log(now.getTime())
